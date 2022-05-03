0fb30c1b8347c241f59b667ea7772c619349bddd
eli <eli@ehsai.ca>
2022-05-02 20:53:51 -0700

    add season by season performance computation, update dependencies, enhance result saving

--- ping_pong.py
+++ ping_pong.py
@@ -6,7 +6,7 @@ import os
 
 import pandas as pd
 
-from score_processor import segment_games_by_player, compute_overall_performance
+from score_processor import *
 
 
 SHEETS = [
@@ -19,20 +19,30 @@ SHEETS = [
 def load_scores():
     scores = []
     for sheet in SHEETS:
-        scores.append(pd.read_excel('scores/scores.xlsx', sheet))
+        season_scores = pd.read_excel('scores/scores.xlsx', sheet)
+
+        season = sheet.split()[0]
+        season_scores['season'] = [season]*season_scores.shape[0]
+
+        scores.append(season_scores)
  
     scores = pd.concat(scores)
     
-    scores.columns = ['idx', 'date', 'location', 'table', 'winner', 'winner_score', 'loser', 'loser_score']
+    scores.columns = ['idx', 'date', 'location', 'table', 'winner', 'winner_score', 'loser', 'loser_score', 'season']
     del scores['idx']
 
     scores = scores.dropna()
 
     return scores
 
-def save_results(data, name):
-    filepath = os.path.join('results', f'{name}.xlsx')
-    data.to_excel(filepath, sheet_name=name, index=False)
+def save_results(dataframes, sheet_names):
+    filepath = os.path.join('results', 'processed_scores.xlsx')
+
+    writer = pd.ExcelWriter(filepath, engine='xlsxwriter')
+    for df, sheet in zip(dataframes, sheet_names):
+        df.to_excel(writer, sheet_name=sheet, index=False)
+
+    writer.save()
 
 
 if __name__ == '__main__':
@@ -41,18 +51,32 @@ if __name__ == '__main__':
     scores = load_scores()
     games_by_player = segment_games_by_player(scores)
 
+    print('---------------------- OVERALL PERFORMANCE ----------------------\n')
     overall_performance = compute_overall_performance(games_by_player)
-
-    print('OVERALL PERFORMANCE\n')
     print(f'{overall_performance}\n')
 
-    #TODO: performance by season
+    print('---------------------- PERFORMANCE BY SEASON --------------------\n')
+    seasons = [sheet.split()[0] for sheet in SHEETS]
+    performances_by_season = compute_season_by_season_performance(games_by_player, seasons)
+
+    for i, season in enumerate(seasons):
+        print(f'\t\t\t{season} SEASON\n')
+        print(f'{performances_by_season[i]}\n')
+
 
     #TODO: head to head performance:
     #   average points for per game against opponent x
     #   average points against per game against opponent x
     #   win % against opponent x
 
-    save_results(overall_performance, 'overall_performance')
+    print('---------------------- SAVING RESULTS ---------------------------\n')
+
+    results = [overall_performance]
+    results.extend(performances_by_season)
+
+    sheet_names = ['overall_performance']
+    sheet_names.extend(seasons)
+
+    save_results(results, sheet_names)
 
     print('PROCESSING SCORES: COMPLETE')
--- score_processor.py
+++ score_processor.py
@@ -9,7 +9,8 @@ IDX_MAP = {
     'winner': 3,
     'winner_score': 4,
     'loser': 5,
-    'loser_score': 6
+    'loser_score': 6,
+    'season': 7
 }
 
 
@@ -28,6 +29,7 @@ def segment_games_by_player(scores):
                 player_games.append({
                     'player': player,
                     'date': date,
+                    'season': scores.iloc[i, IDX_MAP['season']],
                     'player_score': scores.iloc[i, IDX_MAP['winner_score']],
                     'opponent_score': scores.iloc[i, IDX_MAP['loser_score']],
                     'win/loss': 'win',
@@ -38,13 +40,14 @@ def segment_games_by_player(scores):
                 player_games.append({
                     'player': player,
                     'date': date,
+                    'season': scores.iloc[i, IDX_MAP['season']],
                     'player_score': scores.iloc[i, IDX_MAP['loser_score']],
                     'opponent_score': scores.iloc[i, IDX_MAP['winner_score']],
                     'win/loss': 'loss',
                     'opponent': scores.iloc[i, IDX_MAP['winner']].lower()
                 })
 
-        games_by_player[player] = pd.DataFrame(player_games, columns=['date', 'player', 'player_score', 'opponent_score', 'win/loss', 'opponent'])
+        games_by_player[player] = pd.DataFrame(player_games, columns=['date', 'season', 'player', 'player_score', 'opponent_score', 'win/loss', 'opponent'])
 
     return games_by_player
 
@@ -72,3 +75,37 @@ def compute_overall_performance(games_by_player):
     overall_performance = overall_performance.sort_values(by='win %', ascending=False)
 
     return overall_performance
+
+
+def compute_season_by_season_performance(games_by_player, seasons):
+    performance_by_season = {season: [] for season in seasons}
+
+    for season in seasons: 
+        for player, games in games_by_player.items():
+            season_games = games.loc[games['season'] == season]
+
+            if season_games.empty:
+                continue
+
+            win_loss_counts = season_games['win/loss'].value_counts()
+            wins = win_loss_counts.get('win') if not win_loss_counts.get('win') == None else 0
+            losses = win_loss_counts.get('loss') if not win_loss_counts.get('loss') == None else 0
+
+            performance_by_season[season].append({
+                'player': player,
+                'games_played': season_games.shape[0],
+                'wins': wins,
+                'losses': losses,
+                'win %': round(wins/season_games.shape[0] * 100, 2),
+                'avg_pfpg': round(season_games['player_score'].sum() / season_games.shape[0], 2),
+                'avg_papg': round(season_games['opponent_score'].sum() / season_games.shape[0], 2)
+            })
+
+    performance_by_season_dfs = []
+    for season, results in performance_by_season.items():
+        season_performance =  pd.DataFrame(results, columns=['player', 'games_played', 'wins', 'losses', 'win %', 'avg_pfpg', 'avg_papg'])
+        season_performance = season_performance.sort_values(by='win %', ascending=False)
+        
+        performance_by_season_dfs.append(season_performance)
+
+    return performance_by_season_dfs
